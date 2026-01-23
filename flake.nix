dd334c966dfbe12ffb2037149b9c05e240ba931a
Jeremy O'Brien <neutral@fastmail.com>
2026-01-23 08:19:27 -0500

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,35 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
+      flake-utils,
       ...
     }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      lib = pkgs.lib;
-    in
-    {
-      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
-        name = "raptorboost";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # Import nixpkgs for the specific system
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          name = "raptorboost";
 
-        PROTOC = "${pkgs.protobuf}/bin/protoc";
+          PROTOC = "${pkgs.protobuf}/bin/protoc";
 
-        src = lib.cleanSource ./.;
+          src = lib.cleanSource ./.;
 
-        cargoLock.lockFile = ./Cargo.lock;
+          cargoLock.lockFile = ./Cargo.lock;
 
-        meta = {
-          description = "A remote file/directory transfer tool with a simple interface";
-          homepage = "https://github.com/neutralinsomniac/raptorboost";
-          license = lib.licenses.mit;
-          maintainers = [ ];
+          meta = {
+            description = "A remote file/directory transfer tool with a simple interface";
+            homepage = "https://github.com/neutralinsomniac/raptorboost";
+            license = lib.licenses.mit;
+            maintainers = [ ];
+          };
         };
-      };
-    };
+      }
+    );
 }
